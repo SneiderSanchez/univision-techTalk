11cf520768c6c7b5d9ff758afc34c6d088f4748a
Sneider Sanchez <sneiderandres09@hotmail.com>
2024-09-18 18:00:24 -0500

    after techtalk

--- src/script.js
+++ src/script.js
@@ -29,9 +29,47 @@ const gui = new GUI({
   title: "Tech talk univision :')",
   closeFolders: true,
 });
-gui.hide();
+//gui.hide();
 const debugObject = {};
-debugObject.color = "#9900ff";
+debugObject.color = "#ffffff";
+const cube = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2);
+const material = new THREE.MeshBasicMaterial({
+  color: debugObject.color,
+  wireframe: true,
+  //map: colorTexture,
+});
+
+gui.add(material, "wireframe");
+
+gui.addColor(debugObject, "color").onChange(() => {
+  material.color.set(debugObject.color);
+});
+
+debugObject.spin = () => {
+  gsap.to(mesh.rotation, { duration: 4, y: mesh.rotation.y + Math.PI * 2 });
+};
+
+gui.add(debugObject, "spin");
+const mesh = new THREE.Mesh(cube, material);
+scene.add(mesh);
+
+debugObject.zoomIn = () => {
+  gsap.to(mesh.position, { x: 2, duration: 1, delay: 0 });
+  gsap.to(mesh.position, { x: 0, duration: 1, delay: 1.5 });
+};
+
+gui.add(debugObject, "zoomIn");
+debugObject.subdivision = 2;
+
+gui
+  .add(debugObject, "subdivision")
+  .min(1)
+  .max(50)
+  .step(1)
+  .onChange((value) => {
+    mesh.geometry.dispose();
+    mesh.geometry = new THREE.BoxGeometry(1, 1, 1, value, value, value);
+  });
 
 window.addEventListener("resize", () => {
   sizes.width = window.innerWidth;
@@ -57,8 +95,10 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   100
 );
-
-camera.position.z = 3;
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+//camera.position.z = 3;
+camera.position.set(2, 1, 1);
 
 scene.add(camera);
 
@@ -71,6 +111,9 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 function tick() {
   renderer.render(scene, camera);
+  controls.update();
+  camera.lookAt(mesh.position);
+  mesh.rotation.y += 0.01;
   window.requestAnimationFrame(tick);
 }
 tick();
